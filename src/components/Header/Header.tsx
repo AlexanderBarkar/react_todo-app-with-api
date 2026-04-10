f0e47f6bdbec77c06d4b52dfe6b12a322fed49eb
Oleksandr <alexbarkar2000@gmail.com>
2026-04-10 15:13:54 +0300

    added task solution

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,75 +4,47 @@ import { USER_ID } from '../../constants/userdata';
 import { EMPTY_TITLE_ERROR } from '../../constants/errordata';
 
 type Props = {
-  todos: Todo[];
-  inputText: string;
-  tempTodo: Todo | null;
-  setInputText: (value: string) => void;
-  setErrorMessage: (value: string) => void;
-  createTodoHandler: (newTodo: Omit<Todo, 'id'>) => void;
+  newTitle: string;
+  setNewTitle: (value: string) => void;
+  onSubmit: (e: React.FormEvent) => void;
+  onToggleAll: () => void;
+  allCompleted: boolean;
 };
 
 export const Header: React.FC<Props> = ({
-  todos,
-  inputText,
-  tempTodo,
-  setInputText,
-  setErrorMessage,
-  createTodoHandler,
+  newTitle,
+  setNewTitle,
+  onSubmit,
+  onToggleAll,
+  allCompleted,
 }) => {
-  const titleField = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleError = (error: string) => {
-    setErrorMessage(error);
-
-    setTimeout(() => {
-      setErrorMessage('');
-    }, 3000);
-  };
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-
-    if (!inputText.trim()) {
-      handleError(EMPTY_TITLE_ERROR);
-
-      return;
-    }
-
-    if (inputText.trim().length > 0) {
-      const newTodoTemplate = {
-        userId: USER_ID,
-        title: inputText.trim(),
-        completed: false,
-      };
-
-      createTodoHandler(newTodoTemplate);
-    }
-  };
-
+  // 🔥 ВАЖНО: фокус после КАЖДОГО рендера
   useEffect(() => {
-    titleField.current?.focus();
-  }, [todos, tempTodo]);
+    inputRef.current?.focus();
+  });
 
   return (
     <header className="todoapp__header">
       {/* eslint-disable-next-line */}
       <button
         type="button"
-        className="todoapp__toggle-all active"
+        className={`todoapp__toggle-all ${allCompleted ? 'active' : ''}`}
         data-cy="ToggleAllButton"
+        onClick={onToggleAll}
       />
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={onSubmit}>
         <input
+          ref={inputRef}
           type="text"
-          ref={titleField}
-          value={inputText}
+          value={newTitle}
+          autoFocus
           data-cy="NewTodoField"
-          disabled={tempTodo !== null}
           className="todoapp__new-todo"
           placeholder="What needs to be done?"
-          onChange={event => setInputText(event.target.value)}
+          onChange={e => setNewTitle(e.target.value)}
         />
       </form>
     </header>
